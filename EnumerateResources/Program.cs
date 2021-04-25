1cf775ff0749748c97ea09393952449f99ca320b
sonnino@revolution.com.br <Bruno>
2021-04-25 18:51:29 -0400

    Initial commit

---
+++ EnumerateResources/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Windows.Sdk;
+
+var hInst = PInvoke.LoadLibraryEx(@"C:\Windows\Notepad.exe",
+    null, LoadLibraryEx_dwFlags.LOAD_LIBRARY_AS_DATAFILE);
+try
+{
+    PInvoke.EnumResourceTypes(hInst, EnumerateTypes, 0);
+}
+finally
+{
+    PInvoke.FreeLibrary(hInst);
+}
+
+BOOL EnumerateTypes(nint hModule, PWSTR lpType, nint lParam)
+{
+    Console.WriteLine(PwStrToString(lpType));
+    PInvoke.EnumResourceNames(hModule, lpType, EnumNames, 0);
+    return true;
+}
+
+string PwStrToString(PWSTR str)
+{
+    unsafe
+    {
+        return ((ulong)str.Value & 0xFFFF0000) == 0 ?
+            ((ulong)str.Value).ToString() :
+            str.AsSpan().ToString();
+    }
+}
+
+BOOL EnumNames(nint hModule, PCWSTR lpType, PWSTR lpName, nint lParam)
+{
+    Console.WriteLine("  " + PwStrToString(lpName));
+    return true;
+}
---
+++ UseWin32/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Windows.Sdk;
+
+using var handle = PInvoke.FindFirstFile("*.*", out var findData);
+if (handle.IsInvalid)
+    return;
+bool result;
+do
+{
+    Console.WriteLine(ConvertFileNameToString(findData.cFileName.AsSpan()));
+    result = PInvoke.FindNextFile(handle, out findData);
+} while (result);
+
+string ConvertFileNameToString(Span<ushort> span)
+{
+    return string.Join("", span.ToArray().TakeWhile(i => i != 0).Select(i => (char)i));
+}
